6a42c33b1050382426ec9bdf8aac65e958532ca8
NadimRai <rai_nadim@yahoo.com>
2016-09-15 21:45:03 -0700

    edit update show index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+	end
 	def new
 		@article = Article.new
 	end
@@ -16,6 +19,19 @@ class ArticlesController < ApplicationController
 			redirect_to @article
 		else
 			render 'new'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article , notice: "Article was successfully updated"
+		else
+			render 'edit'
 		end
 	end
 
